44f8615e0cb0bb678bd3b2720f8c35bc52affcfd
Bode <jry.bode@gmail.com>
2024-10-16 22:54:37 +0700

    config gone

--- sensors/distance_sensor.py
+++ sensors/distance_sensor.py
@@ -1,36 +1,17 @@
+from gpiozero import DistanceSensor
 import time
-import RPi.GPIO as GPIO
-from config import DISTANCE_MEASUREMENT_INTERVAL
+from config_1 import DISTANCE_MEASUREMENT_INTERVAL
 
 class DistanceSensor:
     def __init__(self, trig_pin, echo_pin):
-        self.trig_pin = trig_pin
-        self.echo_pin = echo_pin
+        self.sensor = DistanceSensor(trigger=trig_pin, echo=echo_pin)
 
     def get_distance(self):
-        GPIO.output(self.trig_pin, GPIO.LOW)
-        time.sleep(0.1)
-
-        GPIO.output(self.trig_pin, GPIO.HIGH)
-        time.sleep(0.00001)
-        GPIO.output(self.trig_pin, GPIO.LOW)
-
-        pulse_start = pulse_end = time.time()
-        timeout = pulse_start + 1.0
-
-        while GPIO.input(self.echo_pin) == GPIO.LOW:
-            pulse_start = time.time()
-            if pulse_start > timeout:
-                return None
-
-        while GPIO.input(self.echo_pin) == GPIO.HIGH:
-            pulse_end = time.time()
-            if pulse_end > timeout:
-                return None
-
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17150
-        return round(distance, 2)
+        try:
+            distance = self.sensor.distance * 100  # Convert to cm
+            return round(distance, 2)
+        except TimeoutError:
+            return None
 
     def run(self, queue, stop_event):
         while not stop_event.is_set():
